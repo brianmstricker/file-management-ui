88a4dfd891d4a7410a3d37e5d987fe1a36d1e8ab
brian <brianmstricker@gmail.com>
2024-03-05 17:01:27 -0500

    feat: checkbox functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import ContextMenu from "@/components/ContextMenu";
 import LeftSide from "@/components/LeftSide";
 import PageTopBar from "@/components/PageTopBar";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CiImageOn } from "react-icons/ci";
 import { VscMusic } from "react-icons/vsc";
-import { FaRegFileZipper } from "react-icons/fa6";
+import { FaRegFileZipper, FaCheck } from "react-icons/fa6";
 import { IoDocumentTextOutline } from "react-icons/io5";
+import { RxDownload, RxUpload } from "react-icons/rx";
+import { cn } from "@/lib/utils";
 
 type File = {
  type: string;
@@ -60,6 +62,7 @@ const initContextMenu = {
 
 export default function Home() {
  const [contextMenu, setContextMenu] = useState(initContextMenu);
+ const [selectedFile, setSelectedFile] = useState<File | null>(null);
  const contextRef = useRef<HTMLDivElement>(null);
  const containerRef = useRef<HTMLDivElement>(null);
  function handleOnContextMenu(e: React.MouseEvent<HTMLDivElement>, file: File) {
@@ -81,12 +84,36 @@ export default function Home() {
    } else {
     y = clientY - contextRefAttr.height - 175;
    }
+   setSelectedFile(file);
    setContextMenu({ show: true, x, y });
   }
  }
  const closeContextMenu = () => {
+  setSelectedFile(null);
   setContextMenu(initContextMenu);
  };
+ useEffect(() => {
+  const allSelected = document.getElementById(
+   "allSelected"
+  ) as HTMLInputElement;
+  const otherFormCheckboxes = document.querySelectorAll(
+   "input[type=checkbox]:not(#allSelected):not(#selectAll)"
+  );
+  otherFormCheckboxes.forEach((checkbox) => {
+   if (checkbox instanceof HTMLInputElement) {
+    checkbox.addEventListener("change", () => {
+     const allOtherCheckboxesChecked = Array.from(otherFormCheckboxes).every(
+      (checkbox) => checkbox instanceof HTMLInputElement && checkbox.checked
+     );
+     if (allOtherCheckboxesChecked) {
+      allSelected.checked = true;
+     } else {
+      allSelected.checked = false;
+     }
+    });
+   }
+  });
+ }, []);
  return (
   <div className="flex-1 h-full flex relative">
    <LeftSide />
@@ -98,13 +125,19 @@ export default function Home() {
     >
      <div className="customGrid items-center px-3 py-4 mb-1">
       <span>
-       <label htmlFor="checkAll" className="border w-4 h-4 block rounded-[4px]">
-        <input
-         type="checkbox"
-         name="checkAll"
-         id="checkAll"
-         className="hidden"
-        />
+       <input
+        type="checkbox"
+        name="allSelected"
+        id="allSelected"
+        className="hidden"
+       />
+       <label
+        htmlFor="allSelected"
+        className="border w-4 h-4 flex items-center justify-center rounded-[4px] pointer-events-none select-none"
+       >
+        <span className="hidden">
+         <FaCheck className="text-white text-[10px]" />
+        </span>
        </label>
       </span>
       <span className="col-span-2">Name</span>
@@ -124,7 +157,11 @@ export default function Home() {
       {files.map((file) => (
        <div
         key={file.name}
-        className="hover:bg-neutral-800/80 transition-all duration-500 cursor-pointer w-full rounded-xl customGrid items-center px-3 py-4 mb-1"
+        className={cn(
+         "transition-all duration-500 cursor-pointer w-full rounded-xl customGrid items-center px-3 py-4 mb-1 ",
+         selectedFile?.name === file.name && "bg-neutral-700",
+         !selectedFile && "hover:bg-neutral-800/80"
+        )}
         ref={contextRef}
         onContextMenu={(e) => handleOnContextMenu(e, file)}
        >
@@ -132,8 +169,12 @@ export default function Home() {
          <input type="checkbox" id={file.name} className="hidden" />
          <label
           htmlFor={file.name}
-          className="border w-4 h-4 block rounded-[4px] cursor-pointer"
-         />
+          className="border w-4 h-4 rounded-[4px] cursor-pointer flex items-center justify-center"
+         >
+          <span className="hidden">
+           <FaCheck className="text-white text-[10px]" />
+          </span>
+         </label>
         </span>
         <div className="col-span-2 flex items-center gap-4">
          <span className="text-2xl">
@@ -163,22 +204,55 @@ export default function Home() {
        </div>
       ))}
      </div>
-     <div className="absolute bottom-[95px] border-t-2 border-t-neutral-800 w-full left-0 py-6">
+     <div className="absolute bottom-[95px] border-t-2 border-t-neutral-800 w-full left-0 py-8">
       <div className="pl-11 customGrid items-center justify-center">
        <span>
+        <input
+         type="checkbox"
+         name="selectAll"
+         id="selectAll"
+         className="hidden"
+        />
         <label
-         htmlFor="checkAll"
-         className="border w-4 h-4 block rounded-[4px]"
+         htmlFor="selectAll"
+         className="border w-4 h-4 flex items-center justify-center rounded-[4px] cursor-pointer select-none"
+         onClick={() => {
+          const selectAll = document.getElementById(
+           "selectAll"
+          ) as HTMLInputElement;
+          const allOtherCheckboxes = document.querySelectorAll(
+           "input[type=checkbox]:not(#selectAll)"
+          );
+          allOtherCheckboxes.forEach((checkbox) => {
+           if (checkbox instanceof HTMLInputElement) {
+            if (selectAll.checked) {
+             checkbox.checked = false;
+            } else {
+             checkbox.checked = true;
+            }
+           }
+          });
+         }}
         >
-         <input
-          type="checkbox"
-          name="checkAll"
-          id="checkAll"
-          className="hidden"
-         />
+         <span className="hidden">
+          <FaCheck className="text-white text-[10px]" />
+         </span>
         </label>
        </span>
-       <span>Select all</span>
+       <span className="col-span-2">Select all</span>
+       <span />
+       <div className="flex col-span-3 gap-8">
+        <span>icon</span>
+        <span className="-ml-3">Speed test:</span>
+        <div className="flex items-center gap-3">
+         <RxDownload className="text-2xl" />
+         <span className="font-bold text-white">10.55 Mbs</span>
+        </div>
+        <div className="flex items-center gap-3">
+         <RxUpload className="text-2xl" />
+         <span className="font-bold text-white">6.30 Mbs</span>
+        </div>
+       </div>
       </div>
      </div>
     </div>
