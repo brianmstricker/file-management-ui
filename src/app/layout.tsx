7ba7fcb2540a50a25488df84b10391505610b475
brian <brianmstricker@gmail.com>
2024-03-06 18:31:47 -0500

    chore: deployment updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import TopBar from "@/components/TopBar";
 
